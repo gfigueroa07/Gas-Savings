a36e2a87076dcabd4eae6d43bfe8b9741974e0a2
gfigueroa07 <guillermofigueroa2840@gmail.com>
2025-08-09 16:31:46 -0400

    .

--- web_scraper.py/scraper.py
+++ web_scraper.py/scraper.py
@@ -1,16 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
-url = "https://www.gasbuddy.com/gasprices/pennsylvania/scranton"
-# url = "https://gasprices.aaa.com/?state=PA"
+url = "https://www.newegg.com/asus-prime-rtx5060ti-o16g-geforce-rtx-5060-ti-16gb-graphics-card-triple-fans/p/N82E16814126795R?Item=N82E16814126795R"
+
 headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36 Edg/138.0.0.0"}
-response = requests.get(url, headers=headers) 
-soup = BeautifulSoup(response.text, 'html.parser')
-# print(soup.prettify())
-gas_price = soup.find_all('div', class_="StationDisplayPrice-module__price___3rARL")
-# gas_price = soup.find_all('div', class_="main-content")
-print(response)
-print(gas_price)
 
-# for price in gas_price:
-#     print(price)
+response = requests.get(url, headers=headers, timeout=15)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+gpu_price = soup.find_all('span', class_='price-current-label')
+price = soup.find_all(string="$")
+parent = price[0].parent
+strong = parent.find("strong")
+final_price = strong.string
+
+print(f"Price for gpu is: {final_price}")
+
